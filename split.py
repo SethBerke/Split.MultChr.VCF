0244a0112251b8bcc508922a16d66084f1719e5b
Seth Berke <sethberke@gmail.com>
2023-07-17 14:37:07 -0400

    All Headerlines added prior to variant information

--- split.py
+++ split.py
@@ -1,5 +1,3 @@
-import linecache
-
 file_path = "./multi.vcf"
 file = open(file_path, "r")
 
@@ -8,12 +6,8 @@ def find_line_with_target(file_path, target):
     last_line_number = 0
     with open(file_path, "r") as file:
         for line_num, line in enumerate(file, start=1):
-            if target == "chr1":
-                if target in line and '#' not in line:
-                    return line_num, linecache.getline(file_path, line_num-1).rstrip('\n')
-            else:
-                if target in line and '#' not in line:
-                    return line_num
+            if target in line and '#' not in line:
+                return line_num
             last_line_number = line_num
     return last_line_number
 
@@ -25,18 +19,15 @@ class Bounds:
              self.upperBound = upperBound
 
 print("HI")
-_,headerLine = find_line_with_target(file_path, "chr1")
-#print("HEADERLINE = ", headerLine)
+header_lower_limit = 0
+header_upper_limit = find_line_with_target(file_path, "chr1") - 1
 
 for i in range(1, 23):
     chromosome = "chr" + str(i)
     chromosomeUB = "chr" + str(i + 1)
     lower_bound = 0
     upper_bound = 0
-    if i == 1:
-        lower_bound, _ = find_line_with_target(file_path, chromosome)
-        upper_bound = find_line_with_target(file_path, chromosomeUB)
-    elif i != 1 and i != 22:
+    if i != 22:
         lower_bound = find_line_with_target(file_path, chromosome)
         upper_bound = find_line_with_target(file_path, chromosomeUB)
         print(find_line_with_target(file_path, chromosome))
@@ -53,15 +44,20 @@ for chromosome, bounds in chrRanges.items():
     print(f"{chromosome} : {bounds.lowerBound} - {bounds.upperBound}")
 
 # WRITE EACH FILE AS A DISTINCT VCF WITH A SINGULAR CHR##
-def writeFiles(input_file_path, output_file_path, lower_limit, upper_limit):
+def writeFiles(input_file_path, output_file_path, lower_limit, upper_limit, header_lower_limit, header_upper_limit):
     with open(input_file_path, "r") as input_file:
         with open(output_file_path, "w") as output_file:
-            output_file.write(headerLine + '\n')  
-            for line_num, line in enumerate(input_file, start=1):
+            header_lines = []
+            for line_num, line in enumerate(input_file, start = 1):
+                if header_lower_limit <= line_num <= header_upper_limit:
+                    header_lines.append(line)  
+            output_file.writelines(header_lines)
+            input_file.seek(0) 
+            for line_num, line in enumerate(input_file, start = 1):
                 if lower_limit <= line_num <= upper_limit:
-                    output_file.write(line)
-
+                    output_file.write(line)  
+            
 for i in range(1, 23):
     chromosome = "chr" + str(i)
     filename = "chr" + str(i) + ".vcf"
-    writeFiles(file_path,filename,chrRanges[chromosome].lowerBound,chrRanges[chromosome].upperBound)
+    writeFiles(file_path,filename,chrRanges[chromosome].lowerBound,chrRanges[chromosome].upperBound, header_lower_limit, header_upper_limit)
