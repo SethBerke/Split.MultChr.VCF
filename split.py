1761367c8141d5c3c5c4a22b725480274e70b162
Seth Berke <sethberke@gmail.com>
2023-07-21 17:53:44 -0400

    Dynamically works for different number of unique chromosomes in initial .vcf file

--- split.py
+++ split.py
@@ -1,47 +1,44 @@
 # FIND LINES ASSOCIATED WITH FIRST OCCURRENCES OF CHR##
-def find_first_occurrence_line_numbers(file_path):
+def chromosomeSplitPoints(file_path):
     first_occurrences = {}
+    unique_chromosomes = set()
     line_number = 0
-
     with open(file_path, "r") as file:
         for line in file:
             line_number += 1
-            if line.startswith("chr"):
-                chromosome = line.split()[0]  # Extract the chromosome name from the line
-
+            if line.startswith("chr") and "#" not in line:
+                chromosome = line.split()[0]  
+                unique_chromosomes.add(chromosome)
                 if chromosome not in first_occurrences:
                     first_occurrences[chromosome] = line_number
+    return first_occurrences, unique_chromosomes
 
-    return first_occurrences
+file_path = "./multi.vcf"
+first_occurrences, unique_chromosomes = chromosomeSplitPoints(file_path)
 
-# DEFINE A DICTIONARY WITH CHR## AS KEYS AND THEIR UPPER AND LOWER BOUND LINE NUMBERS AS VALUES
+# CALCULATE HEADERLINE BOUNDS
+header_lower_limit = 0
+header_upper_limit = min(first_occurrences.values()) - 1
+
+# DEFINE CHR## BOUNDS
 chrRanges = {}
 class Bounds:
         def __init__(self, lowerBound: float, upperBound: float):
              self.lowerBound = lowerBound
              self.upperBound = upperBound
 
-# Call the function to find the first occurrences of each chromosome
-file_path = "./multi.vcf"
-first_occurrences = find_first_occurrence_line_numbers(file_path)
-
-# Calculate header lower and upper limits (assuming it's before the first "chr1" line)
-header_lower_limit = 0
-header_upper_limit = first_occurrences.get("chr1", 1) - 1
-
-# Define the chromosome bounds
-for i in range(1, 23):
-    chromosome = "chr" + str(i)
-    chromosomeUB = "chr" + str(i + 1)
-    lower_bound = first_occurrences.get(chromosome)
-    upper_bound = first_occurrences.get(chromosomeUB, float("inf")) - 1
-
+for chromosome in unique_chromosomes:
+    lower_bound = first_occurrences[chromosome]
+    if len(unique_chromosomes) > 1:
+        upper_bound = first_occurrences.get(next(iter(unique_chromosomes - {chromosome}), float("inf"))) - 1
+    else:
+        upper_bound = float("inf")
     if lower_bound is not None and upper_bound is not None:
         chrRanges[chromosome] = Bounds(lower_bound, upper_bound)
     else:
-        print(f"Target values not found for {chromosome} and/or {chromosomeUB}.")
+        print(f"Target values not found for {chromosome}.")
 
-# WRITE EACH FILE AS A DISTINCT VCF WITH A SINGULAR CHR##
+# WRITE EACH FILE AS A DISTINCT FILE WITH A SINGULAR CHR##
 def writeFiles(input_file_path, output_file_path, lower_limit, upper_limit, header_lower_limit, header_upper_limit):
     with open(input_file_path, "r") as input_file:
         with open(output_file_path, "w") as output_file:
@@ -55,15 +52,23 @@ def writeFiles(input_file_path, output_file_path, lower_limit, upper_limit, head
                 if lower_limit <= line_num <= upper_limit:
                     output_file.write(line)
 
-# Create individual VCF files for each chromosome
-for i in range(1, 23):
-    chromosome = "chr" + str(i)
-    filename = "chr" + str(i) + ".vcf"
-    print("ITERATION: ", i)
-    print("INPUT FILE PATH", file_path)
-    print("OUTPUT FILE NAME", filename)
-    print("CHR LOWER BOUND", chrRanges[chromosome].lowerBound)
-    print("CHR UPPER BOUND", chrRanges[chromosome].upperBound)
-    print("HEADER LOWER LIMIT", header_lower_limit)
-    print("HEADER UPPER LIMIT", header_upper_limit)
+# CREATE INDIVIDUAL .VCF FILES FOR EACH CHR##
+chromosomes_sorted = sorted(unique_chromosomes, key=lambda x: first_occurrences[x])
+num_unique_chromosomes = len(chromosomes_sorted)
+for i in range(len(chromosomes_sorted)):
+    chromosome = chromosomes_sorted[i]
+    lower_bound = first_occurrences[chromosome]
+    if i + 1 < num_unique_chromosomes:
+        upper_bound = first_occurrences[chromosomes_sorted[i + 1]] - 1
+    else:
+        upper_bound = float("inf")
+    chrRanges[chromosome] = Bounds(lower_bound, upper_bound)
+    filename = f"{chromosome}.vcf"
+    print("\nITERATION:", chromosome)
+    print("INPUT FILE PATH:", file_path)
+    print("OUTPUT FILE NAME:", filename)
+    print("CHR LOWER BOUND:", chrRanges[chromosome].lowerBound)
+    print("CHR UPPER BOUND:", chrRanges[chromosome].upperBound)
+    print("HEADER LOWER LIMIT:", header_lower_limit)
+    print("HEADER UPPER LIMIT:", header_upper_limit)
     writeFiles(file_path, filename, chrRanges[chromosome].lowerBound, chrRanges[chromosome].upperBound, header_lower_limit, header_upper_limit)
